a9858f7e97f53bfcbee2f23ba1f9bb3a00df361f
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-03-21 11:44:43 +0900

    fix repo 2

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -12,12 +12,17 @@ const postShotData = async (
       rects.map((rect) => ({ x: rect[0], y: rect[1], w: rect[2], h: rect[3] })),
     ),
   )
-  const res = await axios.post('http://image-processing.work/image', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-      // ...(formData as any).getHeaders(),
+  const res = await axios.post(
+    'https://image-processing.work/image',
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Access-Control-Allow-Origin': '*',
+        // ...(formData as any).getHeaders(),
+      },
     },
-  })
+  )
 
   console.log(res)
 }
